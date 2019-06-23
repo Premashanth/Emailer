3c2d4f0d113263ae05fb0f18d61341750a68e0a8
Your Name <shanthchitti@gmail.com>
2019-06-23 22:21:58 +0530

    proxy added1

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleclientSecret,
-			callbackURL: '/auth/google/callback',
+			callbackURL: 'https://ancient-scrubland-49023.herokuapp.com/auth/google/callback',
 			proxy: true,
 		},
 		(accessToken, refreshToken, profile, done) => {
