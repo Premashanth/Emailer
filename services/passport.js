cadfb780b1ef88561a5018182684a91647f0b439
Your Name <shanthchitti@gmail.com>
2019-06-23 22:12:38 +0530

    proxy added

--- services/passport.js
+++ services/passport.js
@@ -20,6 +20,7 @@ passport.use(
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleclientSecret,
 			callbackURL: '/auth/google/callback',
+			proxy: true,
 		},
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id }).then(existingUser => {
